bc76d0fb4e545c890b2ec898fef89993de175174
PietroDub <pietro.henriquevito@gmail.com>
2025-09-07 21:51:14 -0300

    testando mudanças e animações

--- src/script.js
+++ src/script.js
@@ -6,26 +6,17 @@ const scene = new THREE.Scene()
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red", wireframe: true})
 
 const cubeMesh = new THREE.Mesh( cubeGeometry, cubeMaterial)
-const cubeMesh2 = new THREE.Mesh( cubeGeometry, cubeMaterial)
-const cubeMesh3 = new THREE.Mesh( cubeGeometry, cubeMaterial)
-cubeMesh2.position.x = 2;
-cubeMesh3.position.x = -2;
 
-const group = new THREE.Group();
-group.add(cubeMesh);
-group.add(cubeMesh2);
-group.add(cubeMesh3);
-
-scene.add(group)
-// scene.add(cubeMesh) 
-cubeMesh3.scale.setScalar(2);
-cubeMesh.scale.set(1.5, 1.5, 1.5)
+scene.add(cubeMesh) 
+// cubeMesh3.scale.setScalar(2);
+// cubeMesh.scale.set(1.5, 1.5, 1.5)
 
 const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+//adiciona o axes para seguir o cubo
+cubeMesh.add(axesHelper);
 
 //initialize the camera (perspective)
 const camera = new THREE.PerspectiveCamera(
@@ -33,13 +24,6 @@ const camera = new THREE.PerspectiveCamera(
   window.innerWidth / window.innerHeight,
   0.5, //near
   200)  //far
-
-// const aspectRatio = window.innerWidth / window.innerHeight;
-
-// //initialize Orthohraphic camera
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 200
-// )
 
 camera.position.z = 5;
 cubeMesh.position.distanceTo(camera.position)
@@ -65,7 +49,23 @@ window.addEventListener('resize', () =>{
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
+//initialize the clock
+const clock = new THREE.Clock()
+let previousTime = 0;
+
+//render the scene
 const renderloop = () => {
+
+  //fazendo com q a animação tenha uma velocidade
+  const currentTime = clock.getElapsedTime();
+  const delta = currentTime - previousTime;
+  previousTime = currentTime;
+
+  cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 20;
+  
+  Math.sin(currentTime);
+
+  cubeMesh.scale.x = Math.sin(currentTime);
 
   controls.update();
   renderer.render(scene, camera);
